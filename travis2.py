1cd33c6bbde246838b757d77705c3b091e9b3ebc
Angela Norris <cocoanorris@yahoo.com>
2019-01-30 15:50:48 -0500

    Code to remove name from list using if,lower and remove

--- travis2.py
+++ travis2.py
@@ -1,13 +1,19 @@
-known_users = ["Alice", "Bob", "Claire",
-               "Dan", "Emma", "Fred", "Georgie", "Harry"]
-print(len(known_users))
+known_users = ["Alice","Bob", "Claire", "Dan","Emma","Fred", "Georgie","Harry"]
+
+
 while True:
     print("Hi! My is Travis")
-    name = input("What is your name?: ").strip().capitalize()
+    name = input ("What is your name?: ").strip().capitalize()
 
     if name in known_users:
+        #print("Hello{}!".format(name)) instructor solution
         string = "Hi {}"
         output = string.format(name)
         print(output)
+        remove = input("Would you like to be removed from the system (y/n)?: ").lower()
+        if remove == "y":
+            print(known_users)
+            known_users.remove(name)#only removes first occurance and actually changes original list
+            print(known_users)
     else:
         print("name NOT recognised")
