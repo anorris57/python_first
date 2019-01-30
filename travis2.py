558f05d2973e4273cd3a3b7e43d9a8443727448f
Angela Norris <cocoanorris@yahoo.com>
2019-01-30 16:08:51 -0500

    Using the append command to add to list and adding logic via if/elif

--- travis2.py
+++ travis2.py
@@ -6,14 +6,20 @@ while True:
     name = input ("What is your name?: ").strip().capitalize()
 
     if name in known_users:
-        #print("Hello{}!".format(name)) instructor solution
+        #print("Hello{}!".format(name))
         string = "Hi {}"
         output = string.format(name)
         print(output)
-        remove = input("Would you like to be removed from the system (y/n)?: ").lower()
+        remove = input("Would you like to be removed from the system (y/n)?: ").strip().lower()
         if remove == "y":
-            print(known_users)
             known_users.remove(name)#only removes first occurance and actually changes original list
-            print(known_users)
+            # can use del command to remove element at index and slices del example[0:2]
+        elif remove == 'n':
+            print("No problem, I didn't want you to leave anyway!")
     else:
-        print("name NOT recognised")
+        print("Hmmm I don't think  I have met you yet {}".format(name))
+        add_me = input("Would you like to be added to the system (y/n)?: ").strip().lower()
+        if add_me == "y":
+            known_users.append(name)
+        elif add_me == "n":
+            print("No worries, see you around!")
